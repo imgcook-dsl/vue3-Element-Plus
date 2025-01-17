851607e37456a3f342a61389e0eb190ef8ca3f56
liuchang <chary.liu@outlook.com>
2025-01-17 10:35:26 +0800

    feat: 官方vue-dsl接入

---
+++ code/index.vue
@@ -0,0 +1,310 @@
+<template>
+  <div class="flex-col page">
+    <div class="justify-center block">
+      <img
+        class="banner"
+        :autoScaling="false"
+        src="https://img.alicdn.com/imgextra/i1/O1CN010KWzEW1agT3VsEEFm_!!6000000003359-2-tps-1500-920.png"
+      />
+    </div>
+    <div class="flex-col hd">
+      <div class="justify-between normalDiv">
+        <span class="txt">2:41</span>
+        <div class="justify-center normalDiv1">
+          <img
+            class="mark"
+            :autoScaling="false"
+            src="https://img.alicdn.com/imgextra/i3/O1CN01iI2BOk1sjp2SUKHqD_!!6000000005803-2-tps-70-44.png"
+          />
+          <img
+            class="iconWifi"
+            :autoScaling="false"
+            src="https://img.alicdn.com/imgextra/i4/O1CN017ZG4591Dzwjhnh2FZ_!!6000000000288-2-tps-62-46.png"
+          />
+          <div class="justify-center block1"><div class="color" /></div>
+          <img
+            class="dot"
+            :autoScaling="false"
+            src="https://img.alicdn.com/imgextra/i2/O1CN01OlU1Vw1rY5GzRkvbR_!!6000000005642-2-tps-6-16.png"
+          />
+        </div>
+      </div>
+      <div class="justify-center normalDiv2">
+        <div class="justify-center normalDiv3">
+          <img
+            class="icon"
+            :autoScaling="false"
+            src="https://img.alicdn.com/imgextra/i4/O1CN01pU1fVf1RbaI4QfQ9E_!!6000000002130-2-tps-40-72.png"
+          />
+        </div>
+        <img
+          class="logo"
+          :autoScaling="false"
+          src="https://img.alicdn.com/imgextra/i3/O1CN01oRkMzQ1niNiGNq2Up_!!6000000005123-2-tps-288-84.png"
+        />
+        <div class="justify-center normalDiv4">
+          <img
+            class="icon1"
+            :autoScaling="false"
+            src="https://img.alicdn.com/imgextra/i2/O1CN01Ct89861qkcUz18wRa_!!6000000005534-2-tps-14-14.png"
+          />
+          <img
+            class="icon2"
+            :autoScaling="false"
+            src="https://img.alicdn.com/imgextra/i4/O1CN01KF5ppr27wqXTbdqIB_!!6000000007862-2-tps-14-14.png"
+          />
+          <img
+            class="icon3"
+            :autoScaling="false"
+            src="https://img.alicdn.com/imgextra/i2/O1CN01gdWjLU1RuovMQtQ7U_!!6000000002172-2-tps-14-14.png"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="flex-col bd">
+      <div class="items-center normalDiv5">
+        <span class="word">推荐</span> <span class="text">有趣的店</span>
+        <span class="txt1">小众品牌</span> <span class="word1">发现</span>
+      </div>
+      <div class="colorDiv" />
+    </div>
+    <div class="flex-col main">
+      <div class="color1" />
+      <img
+        class="icon4"
+        :autoScaling="false"
+        src="https://img.alicdn.com/imgextra/i2/O1CN01DrsjTT21jD3AopcTN_!!6000000007020-2-tps-50-42.png"
+      />
+      <div class="items-start normalDiv6">
+        <div class="justify-center normalDiv7">
+          <img
+            class="largeIcon"
+            :autoScaling="false"
+            src="https://img.alicdn.com/imgextra/i1/O1CN01UUEtxR1K3xuEYojNm_!!6000000001109-2-tps-136-110.png"
+          />
+        </div>
+        <div class="flex-col normalDiv8">
+          <span class="shopTitle">快乐原创设计店</span>
+          <div class="items-start block2">
+            <span class="count">9.2</span>
+            <div class="justify-center group">
+              <img
+                class="icon5"
+                :autoScaling="false"
+                src="https://img.alicdn.com/imgextra/i2/O1CN01z8QsQ61LSz9qEDbrT_!!6000000001299-2-tps-20-18.png"
+              />
+            </div>
+            <span class="word2">12.7万粉丝</span>
+          </div>
+        </div>
+        <img
+          class="label"
+          :autoScaling="false"
+          src="https://img.alicdn.com/imgextra/i2/O1CN01lT1OwJ1LFhfoE5vsT_!!6000000001270-2-tps-96-48.png"
+        />
+        <div class="items-center normalDiv9">
+          <span class="txtBenifit">跨店满减</span>
+        </div>
+        <div class="justify-center buttonWrap">
+          <span class="button">进店</span>
+        </div>
+        <img
+          class="icon6"
+          :autoScaling="false"
+          src="https://img.alicdn.com/imgextra/i3/O1CN01vCo1Px1M1Krd10lbX_!!6000000001374-2-tps-36-20.png"
+        />
+      </div>
+      <img
+        class="icon7"
+        :autoScaling="false"
+        src="https://img.alicdn.com/imgextra/i2/O1CN01LM9t0y1azFICUF6lX_!!6000000003400-2-tps-46-48.png"
+      />
+      <img
+        class="logo1"
+        :autoScaling="false"
+        src="https://img.alicdn.com/imgextra/i3/O1CN01GmuUxb1NwsEnj04Nz_!!6000000001635-2-tps-242-160.png"
+      />
+      <div class="items-start normalDiv10">
+        <span class="info">本月女装人气第</span> <span class="num">1</span>
+        <span class="txt2">名</span> <span class="word3">上新快</span>
+        <span class="text1">（8）</span>
+      </div>
+      <span class="txt3">（16）</span> <span class="word4">正品</span>
+      <div class="items-start normalDiv11"><div class="colorDiv1" /></div>
+      <span class="desc">官网正品秋冬蓝色长袖新…</span>
+      <span class="desc1">官网正品袋鼠款黑色长袖…</span>
+      <div class="color2" />
+      <div class="justify-center normalDiv12"><div class="colorDiv2" /></div>
+      <div class="justify-between moneyWrap">
+        <span class="moneyLabel">¥ 260</span> <span class="money">¥ 688</span>
+      </div>
+    </div>
+    <div class="items-center ft">
+      <div class="items-start normalDiv13">
+        <div class="justify-center block3">
+          <div class="flex-col block4">
+            <img
+              class="shop"
+              :autoScaling="false"
+              src="https://img.alicdn.com/imgextra/i4/O1CN016AvRca1qVVQWLNDTC_!!6000000005501-2-tps-728-728.png"
+            />
+            <img
+              class="icon8"
+              :autoScaling="false"
+              src="https://img.alicdn.com/imgextra/i1/O1CN01Qcj78H284dGtYIYLl_!!6000000007879-2-tps-50-42.png"
+            />
+            <div class="items-start block5">
+              <div class="justify-center container">
+                <div class="justify-center normalDiv14">
+                  <div class="color3" />
+                </div>
+              </div>
+              <div class="flex-col block6">
+                <span class="shopTitle1">本相织物潮流服饰店</span>
+                <div class="items-end normalDiv15">
+                  <span class="num1">9.2</span>
+                  <div class="justify-center normalDiv16">
+                    <img
+                      class="largeIcon1"
+                      :autoScaling="false"
+                      src="https://img.alicdn.com/imgextra/i3/O1CN01ilFGFi1zxkOIi0hFj_!!6000000006781-2-tps-100-100.png"
+                    />
+                    <img
+                      class="icon9"
+                      :autoScaling="false"
+                      src="https://img.alicdn.com/imgextra/i2/O1CN01f4WIcl23YnH74RgVK_!!6000000007268-2-tps-20-18.png"
+                    />
+                  </div>
+                  <div class="items-center normalDiv17">
+                    <span class="shopTitle2">5年老店</span>
+                    <span class="txt4">12.7万粉丝</span>
+                  </div>
+                </div>
+              </div>
+              <img
+                class="icon10"
+                :autoScaling="false"
+                src="https://img.alicdn.com/imgextra/i1/O1CN014c0H761dbptZMnczR_!!6000000003755-2-tps-46-48.png"
+              />
+              <div class="justify-center buttonWrap1">
+                <span class="button1">进店</span>
+              </div>
+              <img
+                class="icon11"
+                :autoScaling="false"
+                src="https://img.alicdn.com/imgextra/i1/O1CN01IIqJf11y2fPaFmgHX_!!6000000006521-2-tps-36-20.png"
+              />
+            </div>
+            <div class="items-start txtBgWrap">
+              <img
+                class="txtBg"
+                :autoScaling="false"
+                src="https://img.alicdn.com/imgextra/i4/O1CN01agltW11MV6boGIvAw_!!6000000001439-2-tps-676-100.png"
+              />
+              <span class="text2">上新快</span>
+              <span class="txt5">90后偏爱</span>
+              <span class="word5">（8）</span>
+              <span class="text3">（16）</span> <span class="txt6">正品</span>
+            </div>
+            <div class="justify-center block7"><div class="colorDiv3" /></div>
+          </div>
+          <div class="flex-col layerWrap">
+            <img
+              class="layer"
+              :autoScaling="false"
+              src="https://img.alicdn.com/imgextra/i2/O1CN01TrreHI1F4MAJod5Ay_!!6000000000433-2-tps-980-1042.png"
+            />
+            <div class="color4" />
+            <span class="desc2">日系复古潮牌国潮流休闲…</span>
+            <div class="justify-between moneyWrap1">
+              <span class="moneyLabel1">¥ 260</span>
+              <span class="money1">¥ 300</span>
+            </div>
+          </div>
+          <span class="desc3">山本风宽松裙裤男可拆卸…</span>
+          <div class="items-center colorDiv4" />
+          <img
+            class="shopLong"
+            :autoScaling="false"
+            src="https://img.alicdn.com/imgextra/i4/O1CN01HxIa071p7rMb2YpPb_!!6000000005314-2-tps-760-1012.png"
+          />
+        </div>
+        <img
+          class="cover"
+          :autoScaling="false"
+          src="https://img.alicdn.com/imgextra/i4/O1CN01GmKt581qtmQ4UTRd1_!!6000000005554-2-tps-1404-600.png"
+        />
+        <img
+          class="banner1"
+          :autoScaling="false"
+          src="https://img.alicdn.com/imgextra/i4/O1CN01QAvMzA1hzt9xdXmCM_!!6000000004349-2-tps-1404-1156.png"
+        />
+        <div class="items-start normalDiv18">
+          <div class="flex-col block8">
+            <div class="items-center block9">
+              <span class="text4">极简轻生活</span>
+            </div>
+            <span class="title">早安，想和你喝一杯咖啡聊聊人生</span>
+            <div class="items-center group1">
+              <div class="justify-center container1">
+                <img
+                  class="icon12"
+                  :autoScaling="false"
+                  src="https://img.alicdn.com/imgextra/i3/O1CN0139v9oL1vsvlPMBKqf_!!6000000006229-2-tps-52-48.png"
+                />
+              </div>
+              <span class="word6">zakka日系小铺</span>
+            </div>
+          </div>
+        </div>
+        <div class="flex-col normalDiv19">
+          <div class="justify-center storeWrap">
+            <img
+              class="store"
+              :autoScaling="false"
+              src="https://img.alicdn.com/imgextra/i2/O1CN01BP8i5b20Q8wsflfrF_!!6000000006843-2-tps-378-374.png"
+            />
+          </div>
+          <div class="items-center block10"><div class="color5" /></div>
+        </div>
+      </div>
+      <div class="justify-center shopWrap">
+        <img
+          class="shop1"
+          :autoScaling="false"
+          src="https://img.alicdn.com/imgextra/i4/O1CN01A74ZPC1ji92nAWw8z_!!6000000004581-2-tps-420-420.png"
+        />
+        <div class="items-center storeWrap1">
+          <img
+            class="store1"
+            :autoScaling="false"
+            src="https://img.alicdn.com/imgextra/i1/O1CN01p4kOSM1s1r1801aUi_!!6000000005707-2-tps-390-360.png"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+
+
+    export default {
+      components: {
+
+      },
+      data() {
+        return {
+          "items":[{"HELKO":"2"}],
+constants: {}
+        }
+      },
+      methods: {
+
+      },
+
+    }
+</script>
+
+<style src="./index.css" />
+;
