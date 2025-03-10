3e5eec01da61486ff61cd4d17e5b919d414b59c8
liuchang <chary.liu@outlook.com>
2025-03-10 09:15:55 +0800

    feat: dsl发布npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,38 @@
-const path = require('path');
-
-module.exports = {
-  // watch: true,
-  entry: './src/core/entry.ts',
+const path = require("path");
+const commonCfg = {
+  entry: "./src/core/entry.ts",
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use:'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
-    },
-
-    ]
+      },
+    ],
   },
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'src'),
-    libraryTarget: 'commonjs2',
-  }
 };
+module.exports = [
+  {
+    ...commonCfg,
+    output: {
+      filename: "index.js",
+      path: path.resolve(__dirname, "src"),
+      libraryTarget: "commonjs2",
+    },
+  },
+  {
+    ...commonCfg,
+    output: {
+      filename: "package/index.umd.js",
+      path: path.resolve(__dirname, "src"),
+      library: {
+        name: 'ParseSchema', // UMD 全局变量名
+        type: 'umd', 
+      },
+      globalObject: 'this',
+    },
+  },
+];
